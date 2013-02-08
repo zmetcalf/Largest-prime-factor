4e1fc713cd3710ef2eaf0f85c11a4d45b514fe40
Zachery Metcalf <zachery.metcalf@gmail.com>
2013-02-08 16:08:06 -0500

    Found possible solution

--- Largest-Prime-Factor.py
+++ Largest-Prime-Factor.py
@@ -1,19 +1,19 @@
 primeFactor = 600851475143
-counter = primeFactor - 1
+counter = 2
 
 def CheckPrime(numCheck):
-    i = numCheck - 1
-    while i >= 2:
+    i = 2
+    while i < numCheck:
         if numCheck % i == 0:
             return 0
-        i -= 1
+        i += 1
     return 1
 
-while counter >= 2:
+while counter < primeFactor:
     if (primeFactor % counter == 0):
         if CheckPrime(counter):
             print counter
-    counter -= 1
+    counter += 1
 
 ''' range() has a max
 for i in range(2, primeFactor):
